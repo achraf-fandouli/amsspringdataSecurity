733de2cf66c6c2eeb92e325cbd2e1a9fa83f6381
Achraf FANDOULI <achref.fendouli@gmail.com>
2022-02-24 23:52:11 +0100

    1.f - fix header.html page

--- src/main/java/com/sip/ams/configuration/SecurityConfiguration.java
+++ src/main/java/com/sip/ams/configuration/SecurityConfiguration.java
@@ -42,6 +42,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/login").permitAll() // accès pour tous users
                 .antMatchers("/registration").permitAll() // accès pour tous users
 
+
                 .antMatchers("/provider/**").hasAuthority("ADMIN")
                 .antMatchers("/article/**").hasAuthority("USER").anyRequest()
                 .authenticated().and().csrf().disable().formLogin() // l'accès de fait via un formulaire
